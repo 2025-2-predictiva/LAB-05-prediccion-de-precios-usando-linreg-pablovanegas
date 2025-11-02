22f119bfad81d4ee9ac665acffe113789726dc4c
pablovanegas <juvanegas@unal.edu.co>
2025-11-02 04:36:23 -0500

    yup

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -25,14 +25,14 @@ METRICS = [
         "type": "metrics",
         "dataset": "train",
         "r2": 0.2,
-        "mse": 2.2,
+        "mse": 0.2,
         "mad": 0.600,
     },
     {
         "type": "metrics",
         "dataset": "test",
         "r2": 0.128,
-        "mse": 2.10,
+        "mse": 0.10,
         "mad": 0.430,
     },
 ]
